f96acd89cde4699e39a263a9f10906d84abe075a
Numan Safi <numansafi@live.com>
2024-05-01 23:57:43 +0100

    learn c# fundamentals

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine(a + b);
+
     }
 }
