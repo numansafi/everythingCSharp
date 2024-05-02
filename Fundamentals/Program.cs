5c7e74ad308d4342b1e89c716ed8b77272aba1ab
Numan Safi <numansafi@live.com>
2024-05-02 21:00:06 +0100

    method return a type

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -13,11 +13,17 @@ class Program
         PrintSelectedOption(seeTodos);
         var userInput = Console.ReadLine();
         Console.WriteLine($"User input: {userInput}");
+
+        Console.WriteLine(Add(1,2));
         
     }
 
     private static void PrintSelectedOption(string selectedOption)
     {
         Console.WriteLine($"Selected Option: {selectedOption}");
+    }
+    private static int Add(int a, int b)
+    {
+        return a + b;
     }
 }
