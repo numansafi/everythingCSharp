ce02a8a18ea11baa3c144454dab553046d2e2329
Numan Safi <numansafi@live.com>
2024-05-02 20:42:14 +0100

    wip: going through c# fundamentals

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -5,10 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        int a = 5;
-        int b = 10;
-
+        var a = 5;
+        var b = 10;
         Console.WriteLine(a + b);
+
+        Console.WriteLine("[S]ee all Todo items");
+        var userInput = Console.ReadLine();
+        Console.WriteLine($"User input: {userInput}");
 
     }
 }
