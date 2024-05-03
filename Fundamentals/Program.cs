d0301a585a2890842eed96492d65a483fad382ec
Numan Safi <numansafi@live.com>
2024-05-03 23:49:23 +0100

    Arrays, for and foreach loops

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Fundamentals;
+﻿using Microsoft.VisualBasic;
+
+namespace Fundamentals;
 
 class Program
 {
@@ -15,7 +17,7 @@ class Program
         // Console.WriteLine($"User input: {userInput}");
         //
         // Console.WriteLine(Add(1,2));
-        
+
         // Parsing
         // Console.WriteLine("Provide a number: ");
         // var userInput = Console.ReadLine()!;
@@ -28,22 +30,73 @@ class Program
         // {
         //     number++;
         //     Console.WriteLine($"Number: {number}");
-        
+
         // }
 
-        for (int i = 1; i < 20; i++)
-        {
-            if (i % 4 == 0)
-            {
-                continue;
-            }
-            Console.WriteLine(i);
-        }
+        // for (int i = 1; i < 20; i++)
+        // {
+        //     if (i % 4 == 0)
+        //     {
+        //         continue;
+        //     }
+        //     Console.WriteLine(i);
+        // }
 
-
+        var numbers = new int[] { 1, 2, 3, 4 };
+        System.Console.WriteLine(IsWordPresentInCollection(new[] {"one", "two", "three"}, "three"));
     }
 
-     public static int Factorial(int number)
+    public static bool IsAnyWordLongerThan(int length, string[] words)
+    {
+        foreach (var word in words)
+        {
+            if (word.Length > length) return true;
+        }
+
+        return false;
+    }
+
+    public static int FindMax(int[,] numbers)
+    {
+        var arr1 = numbers.GetLength(0);
+        var arr2 = numbers.GetLength(1);
+
+        if ( arr1 == 0 || arr2 == 0) return -1;
+
+        int max = numbers[0,0];
+
+        for (int i = 0; i < arr1; i++)
+        {
+            for (int j = 0; j < arr2; j++)
+            {
+                if (numbers[i,j] > max) max = numbers[i,j];
+            }
+        }
+
+        return max;
+    }
+
+    public static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+    {
+        foreach (var word in words)
+        {
+            if ( word == wordToBeChecked) return true;
+        }
+
+        return false;
+    }
+    public static string BuildHelloString()
+    {
+        var letters = new char[] { 'h','e','l','l','o'};
+        var result = "";
+        for (int i = 0; i < letters.Length; ++i)
+        {
+           result += letters[i];
+        }
+        return result;
+    }
+
+    public static int Factorial(int number)
     {
         int sum = 1;
         for (int i = 1; i <= number; i++)
@@ -63,9 +116,9 @@ class Program
             str += character;
             count++;
         } while (count < targetLength);
-            
-            return str;
-        }
+
+        return str;
+    }
 
     private static void PrintSelectedOption(string selectedOption)
     {
