6f4d409dfc915e74cead0a7875315833a31a6190
Numan Safi <numansafi@live.com>
2024-05-03 23:16:22 +0100

    loops, break and continue

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -17,13 +17,55 @@ class Program
         // Console.WriteLine(Add(1,2));
         
         // Parsing
-        Console.WriteLine("Provide a number: ");
-        var userInput = Console.ReadLine()!;
-        var number = int.Parse(userInput);
-        Console.WriteLine(number);
+        // Console.WriteLine("Provide a number: ");
+        // var userInput = Console.ReadLine()!;
+        // var number = int.Parse(userInput);
+        // Console.WriteLine(number);
+
+        //LOOPS
+        // int number = 0;
+        // while (number < 10)
+        // {
+        //     number++;
+        //     Console.WriteLine($"Number: {number}");
+        
+        // }
+
+        for (int i = 1; i < 20; i++)
+        {
+            if (i % 4 == 0)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+        }
 
 
     }
+
+     public static int Factorial(int number)
+    {
+        int sum = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            sum *= i;
+        }
+
+        return sum;
+    }
+
+    public static string RepeatCharacter(char character, int targetLength)
+    {
+        string str = "";
+        int count = 0;
+        do
+        {
+            str += character;
+            count++;
+        } while (count < targetLength);
+            
+            return str;
+        }
 
     private static void PrintSelectedOption(string selectedOption)
     {
