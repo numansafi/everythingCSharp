da2841d8d1761caae022cdcfa3e7522359faabc9
Numan Safi <numansafi@live.com>
2024-05-15 21:11:03 +0100

    DateTime class

--- ObjectOrientedPrograming/Program.cs
+++ ObjectOrientedPrograming/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// OOP
+
+// An object(instance) of class DateTime has created
+var pizzaDay23 = new DateTime(2023, 02, 09);
+System.Console.WriteLine(pizzaDay23.DayOfWeek);
+
+var pizzaDay24 = pizzaDay23.AddYears(1);
+System.Console.WriteLine(pizzaDay24.DayOfWeek);
+
