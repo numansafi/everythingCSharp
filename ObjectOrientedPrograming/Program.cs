19d8ac1483bb99d935f2f84686b03f67d0dc9655
Numan Safi <numansafi@live.com>
2024-05-15 22:02:45 +0100

    first class

--- ObjectOrientedPrograming/Program.cs
+++ ObjectOrientedPrograming/Program.cs
@@ -2,8 +2,29 @@
 
 // An object(instance) of class DateTime has created
 var pizzaDay23 = new DateTime(2023, 02, 09);
-System.Console.WriteLine(pizzaDay23.DayOfWeek);
+//Console.WriteLine(pizzaDay23.DayOfWeek);
 
 var pizzaDay24 = pizzaDay23.AddYears(1);
-System.Console.WriteLine(pizzaDay24.DayOfWeek);
+//Console.WriteLine(pizzaDay24.DayOfWeek);
+
+// First class
+
+var rectangle = new Rectangle(10, 5);
+System.Console.WriteLine(rectangle.Height);
+
+class Rectangle
+{
+  // Fields of a class
+  public int Width;
+  public int Height;
+
+  // Constructor
+  public Rectangle(int width, int height)
+  {
+    Width = width;
+    Height = height;
+  }
+}
+
+
 
