c91120283583e3f36f28b11f08a566856bb70033
Numan Safi <numansafi@live.com>
2024-05-16 21:41:31 +0100

    overloading methods and constructors

---
+++ ObjectOrientedPrograming/MedicalAppointment.cs
@@ -0,0 +1,31 @@
+﻿namespace ObjectOrientedPrograming;
+
+public class MedicalAppointment
+{
+  private string _patientName;
+  private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    public MedicalAppointment(string patientName)
+    {
+      _patientName = patientName;
+      _date = DateTime.Now.AddDays(7);
+    }
+
+    public void Reschedule(DateTime date)
+    {
+      _date = date;
+    }
+
+    public void Reschedule(int month, int day)
+    {
+      _date = new DateTime(_date.Year, month, day);
+    }
+
+
+}
