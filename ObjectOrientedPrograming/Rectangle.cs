7d7f136ff58d263f6275b2aaec8b13fdabe62916
Numan Safi <numansafi@live.com>
2024-05-18 16:15:25 +0100

    getter and setter methods

--- ObjectOrientedPrograming/Rectangle.cs
+++ ObjectOrientedPrograming/Rectangle.cs
@@ -2,21 +2,24 @@ class Rectangle
 {
   // Fields of a class
   public int Width;
-  public int Height;
+  private int _height;
 
   // Constructor
   public Rectangle(int width, int height)
   {
     Width = width;
-    Height = height;
+    _height = height;
   }
+
+  public int GetHeight() => _height;
+  public void setHeight(int value) => _height = value;
 } 
 
 class ShapeMeasurementCalculator()
 {
-  public int CalculateRectanglePerimeter(Rectangle rectangle ) =>  2 * rectangle.Width + 2 * rectangle.Height;
+  public int CalculateRectanglePerimeter(Rectangle rectangle ) =>  2 * rectangle.Width + 2 * rectangle.GetHeight();
 
-  public int CalculateArea(Rectangle rectangle) => rectangle.Width * rectangle.Height;
+  public int CalculateArea(Rectangle rectangle) => rectangle.Width * rectangle.GetHeight();
 }
  
 
