37c0211c2d61e7dc834d758bee02044dd6c44e54
Numan Safi <numansafi@live.com>
2024-06-05 21:20:37 +0100

    virtual methods practice

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -7,6 +7,14 @@ Console.WriteLine(cheddar.Name);
 Ingredient ingredient = new Cheddar();
 Console.WriteLine(ingredient.Name);
 
+var numbers = new List<int> {1, 4, 6, -1, 12, 44, -8, -19};
+
+var allNumberCalculator = new NumbersSumCalculator();
+System.Console.WriteLine(allNumberCalculator.Calculate(numbers));
+
+var positiveNumberCalculator = new PositiveNumbersSumCalculator();
+Console.WriteLine(positiveNumberCalculator.Calculate(numbers));
+
 // Inheritance
 
 public class Ingredient
@@ -46,3 +54,33 @@ public class Pizza
     return $"This is a pizza with {string.Join(", ", _ingredients)}";
   }
 }
+
+public class NumbersSumCalculator
+{
+  public int Calculate(List<int> numbers)
+  {
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+      if (ShallBeAdded(number))
+      {
+        sum += number;
+      }
+    }
+    return sum;
+  }
+
+  protected virtual bool ShallBeAdded(int number)
+  {
+    return true;
+  }
+}
+
+public class PositiveNumbersSumCalculator : NumbersSumCalculator
+{
+    protected override bool ShallBeAdded(int number)
+    {
+      return number > 0;
+    }
+}
+
